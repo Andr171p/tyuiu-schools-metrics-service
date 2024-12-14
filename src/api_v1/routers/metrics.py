827e91aca2a4ba910709b6fbd926082c4753de73
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-14 15:32:57 +0500

    [schools-metrics-service] 14.12.2024 add routers

--- src/api_v1/routers/metrics.py
+++ src/api_v1/routers/metrics.py
@@ -5,8 +5,8 @@ from fastapi.exceptions import HTTPException
 from src.database.services.school import school_service
 from src.database.services.direction import direction_service
 
-from src.api_v1.schemas.metrics import GetMetricResponse
-from src.metrics import school_metrics
+from src.api_v1.schemas.metrics import MetricSchema, GetMetricResponse
+from src.metrics import utils
 
 
 metrics_router = APIRouter(
@@ -22,5 +22,22 @@ async def get_school_metrics(school_id: int) -> JSONResponse:
         raise HTTPException(
             ...
         )
+    applicants = school.applicants
     directions = await direction_service.get_directions_by_school_id(school_id)
-    ...
+    metrics = MetricSchema(
+        applicants_count=utils.get_applicants_count(applicants),
+        students_count=utils.get_students_count(directions),
+        avg_gpa=utils.get_avg_gpa(applicants),
+        avg_score=utils.get_avg_score(applicants),
+        popular_universities=utils.get_popular_universities(directions, 3),
+        popular_directions=utils.get_popular_directions(directions, 3)
+    )
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "data": {
+                "status": "ok",
+                "metrics": metrics
+            }
+        }
+    )
